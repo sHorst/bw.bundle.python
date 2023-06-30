bc1487abd12c74baa329b6a17fb245be9dd200b6
Stefan Horst <stefan@ultrachaos.de>
2023-06-30 22:33:17 +0200

    pip is now python3

--- items.py
+++ items.py
@@ -6,10 +6,13 @@ for pkg, config in node.metadata.get('python', {}).get('packages', {}).items():
         # TODO: add dependency for update apt_cache
         pkg_pip[pkg] = config
 
-symlinks = {
-    '/usr/bin/pip': {
-        "target": '/usr/bin/pip3',
-        "group": "root",
-        "owner": "root",
-    }
-}
+if node.os == 'debian':
+    # starting debian 12 /usr/bin/pip is python3
+    if node.os_version[0] < 12:
+        symlinks = {
+            '/usr/bin/pip': {
+                "target": '/usr/bin/pip3',
+                "group": "root",
+                "owner": "root",
+            }
+        }
